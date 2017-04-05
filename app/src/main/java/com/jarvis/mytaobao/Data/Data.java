807b46284a17113529ce4d3e74b66bdfd7873ad9
THtianhao <356831631@qq.com>
2017-04-05 23:18:57 +0800

    first commit

---
+++ app/src/main/java/com/jarvis/mytaobao/Data/Data.java
@@ -0,0 +1,15 @@
+package com.jarvis.mytaobao.Data;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+
+/**
+ * @author http://yecaoly.taobao.com
+ *
+ */
+public class Data  {
+	public static int arrayList_cart_id=0;
+	public static ArrayList<HashMap<String, Object>> arrayList_cart=new ArrayList<HashMap<String,Object>>();
+    public static float Allprice_cart=0;
+}
---
+++ app/src/main/java/com/jarvis/mytaobao/cart/StockBaby_F.java
@@ -0,0 +1,31 @@
+package com.jarvis.mytaobao.cart;
+
+import com.jarvis.mytaobaotest.R;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * @author http://yecaoly.taobao.com
+ *
+ */
+public class StockBaby_F extends Fragment {
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+		View view=LayoutInflater.from(getActivity()).inflate(R.layout.cart_stock_f, null);
+		initView(view);
+		return view;
+	}
+	
+	private void initView(View view){
+		
+		
+		
+	}
+
+}
