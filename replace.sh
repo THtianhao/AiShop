e7f7b59294fbc1893e1e1e3557db55b1722fad1d
THtianhao <356831631@qq.com>
2017-04-08 19:22:54 +0800

    修改shell

--- replace.sh
+++ replace.sh
@@ -1,23 +1,25 @@
 #!/bin/bash
-filetext=$(cat /home/ubuntu/thefile.txt)
 echo $filetext
-echo ${filetext:0:1}
-echo ${filetext:1:1}
-echo ${filetext:2:1}
-echo ${filetext:3:1}
-if [ ${filetext:0:1} = 1 ] ; then
+p1= cut -b 1 /home/ubuntu/thefile.txt
+p2= cut -b 2 /home/ubuntu/thefile.txt
+p3= cut -b 3 /home/ubuntu/thefile.txt
+p4= cut -b 4 /home/ubuntu/thefile.txt
+echo $p1 $p2 $p3 $p4
+
+if [ $p1 = 1 ] ; then
+		echo 'aaaaa'
         myvar="my_gridview"
         find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
 fi
-if [ ${filetext:1:1} = 1 ] ; then
+if [ $p2 = 1 ] ; then
         myvar="my_linearlayout1"
         find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
 fi
-if [ ${filetext:2:1} = 1 ] ; then
+if [ $3
         myvar="my_linearlayout2"
         find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
 fi
-if [ ${filetext:3:1} = 1 ] ; then
+if [ $p4 = 1 ] ; then
         myvar="my_linearlayout3"
         find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
 fi
