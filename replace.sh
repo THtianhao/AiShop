d7ee556febea34cc872d0180ebfabd418bf0a14d
THtianhao <356831631@qq.com>
2017-04-08 19:34:47 +0800

    修改shell

--- replace.sh
+++ replace.sh
@@ -6,20 +6,16 @@ p3= cut -b 3 /home/ubuntu/thefile.txt
 p4= cut -b 4 /home/ubuntu/thefile.txt
 echo $p1 $p2 $p3 $p4
 
-if [ $p1 = 1 ] ; then
-		echo 'aaaaa'
-        myvar="my_gridview"
-        find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
-fi
-if [ $p2 = 1 ] ; then
-        myvar="my_linearlayout1"
-        find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
-fi
-if [ $3
-        myvar="my_linearlayout2"
-        find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
-fi
-if [ $p4 = 1 ] ; then
-        myvar="my_linearlayout3"
-        find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
-fi
+if[$p1=1];
+    echo '1 success'
+    myvar="my_gridview"
+    find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
+if[$p2=1];
+    myvar="my_linearlayout1"
+    find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
+if[$p3=1];
+    myvar="my_linearlayout2"
+    find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
+if[$p4=1];
+    myvar="my_linearlayout3"
+    find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
