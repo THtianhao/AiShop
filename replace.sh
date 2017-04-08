74dfca90445f5c9545df2fd04ad9da1f322544ba
THtianhao <356831631@qq.com>
2017-04-08 16:59:54 +0800

    修改脚本

--- replace.sh
+++ replace.sh
@@ -1,18 +1,23 @@
 #!/bin/bash
-if [ $TOTOVAR1 = 1 ] ; then     
-	myvar="my_gridview"
-find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
+filetext=$(cat /home/ubuntu/thefile.txt)
+echo $filetext
+echo ${filetext:0:1}
+echo ${filetext:1:1}
+echo ${filetext:2:1}
+echo ${filetext:3:1}
+if [ ${filetext:0:1} = 1 ] ; then
+        myvar="my_gridview"
+        find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
 fi
-if [ $TOTOVAR2 = 1 ] ; then   
-	myvar="my_linearlayout1"
-find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
-fi 
-if [ $TOTOVAR3 = 1 ] ; then 
-	myvar="my_linearlayout2"
-find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
+if [ ${filetext:1:1} = 1 ] ; then
+        myvar="my_linearlayout1"
+        find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
 fi
-if [ $TOTOVAR4 = 1 ] ; then
+if [ ${filetext:2:1} = 1 ] ; then
+        myvar="my_linearlayout2"
+        find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
+fi
+if [ ${filetext:3:1} = 1 ] ; then
         myvar="my_linearlayout3"
-find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
+        find ./app/src/ -name 'home_f.xml' | xargs perl -pi -e 's|android:id=\"@\+id\/'$myvar'\"|android:id="@\+id/'$myvar'" android:visibility="gone"|g'
 fi
-
